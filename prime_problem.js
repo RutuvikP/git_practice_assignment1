18756fd9cf2e0c320ad2473f254bbc0d9b14398b
unknown <poojaharsule88@gmail.com>
2022-11-08 22:35:30 +0530

    Prime problem Done

---
+++ prime_problem.js
@@ -0,0 +1,14 @@
+  let a=8;
+  let count=0;
+  for(i=1;i<=a;i++){
+    if(a%i==0){
+      count++;
+    }
+  }
+  if(count==2){
+    console.log(a,"is a Prime Number");
+  } else {
+    console.log(a,"is not a prime number")
+  }
+
+
